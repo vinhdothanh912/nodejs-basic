5fd9bceb080d3a26b610a8f72298455c69f4a035
vinhdothanh <vinh.dothanh912@gmail.com>
2023-05-14 15:40:02 +0700

    install mysqp2, set up db connection

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
+const mysql = require("mysql2");
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -12,6 +13,21 @@ configViewEngine(app);
 
 // set up routers
 app.use("/", webRoutes);
+
+// test connection
+// create the connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  port: 3307,
+  database: "vinhdothanh912",
+  password: "123456",
+});
+
+connection.query("SELECT * FROM Users u", function (err, results, fields) {
+  console.log("@@results=", results); // results contains rows returned by server
+  console.log("@@fields=", fields); // fields contains extra meta data about results, if available
+});
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
